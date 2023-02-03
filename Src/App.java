8ad52f85dcb4e9963cc1a84a538296d024edc292
AdryellenAlves <adryellenalves09@gmail.com>
2023-02-03 18:40:52 -0300

    Subindo os arquivos

---
+++ Src/App.java
@@ -0,0 +1,29 @@
+package Src;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        // TODO Auto-generated method stub
+
+        Parent root = FXMLLoader.load(getClass().getResource("SceneConversor.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        scene.getStylesheets().add("Style.css");
+        stage.setResizable(false);
+        stage.setTitle("Conversor de Temperatura");
+
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
